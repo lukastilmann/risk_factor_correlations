0d98543289e7fdad59117d6bff2ac8aacde6e155
LNhart <lukastilmann97@gmail.com>
2020-11-27 19:24:14 +0100

    updated scraping, but still issues with parsing text left

--- scrape_reports.py
+++ scrape_reports.py
@@ -39,6 +39,8 @@ def is_item_2_header(text):
 
     return False
 
+def print_filing(tuple):
+  print(str(not tuple[3]) + " " + tuple[0] + " - " + tuple[1] + " -" + tuple[2][:250])
 
 def parse_filing(company, doc_lxml, doc):
     text_elem = doc_lxml.xpath("//text()")
@@ -54,8 +56,10 @@ def parse_filing(company, doc_lxml, doc):
             if is_item_1b_header(text) or is_item_2_header(text):
                 fromhere = False
                 if len(risk_factors_text) > 10:
-                    # print("Ended on Item 1B!")
+                    print("got item 2 or 1b and has text!")
                     break
+                else:
+                    print("got next item but no text!")
             text = text.strip()
             if len(text) > 4 and not (
                     any(word in text.lower() for word in ["index", "contents", "item", "factor", "summary"]) and len(
@@ -69,10 +73,15 @@ def parse_filing(company, doc_lxml, doc):
                     current_text = ""
         if is_item_1a_header(text):
             fromhere = True
+            print("item 1a start")
 
     text_content = "\n".join(risk_factors_text)
 
-    return (company, doc.content["Period of Report"], text_content, fromhere)
+    print(fromhere)
+    print(len(text_content))
+    print("--------------------------")
+
+    return (company.name, doc.content["Period of Report"], text_content, fromhere)
 
 def get_company_by_cik(cik):
     cik_table = str(cik)
@@ -90,13 +99,75 @@ def get_filings_by_company(company, type, n):
 
     return (docs_lxml, docs_data)
 
+def pull_reports(cik, ticker, c_id, start):
+
+    company = get_company_by_cik(cik)
+    #yearly reports first
+    '''
+    filings_10k = get_filings_by_company(company, "10-K", 13)
+    #check if enough
+    if pd.Timestamp(str(filings_10k[1][-1].content["Period of Report"])) > pd.Timestamp(year=2005, month=12, day=31):
+        filings_10k = get_filings_by_company(company, "10-K", 26)
+
+    dates = []
+    content = []
+    fromhere = []
+    has_content = []
+
+    for i in range(len(filings_10k[0])):
+        period = pd.Timestamp(str(filings_10k[1][i].content["Period of Report"]))
+        if period < start:
+            break
+        parsed = parse_filing(company, filings_10k[0][i], filings_10k[1][i])
+        dates.append(period)
+        content.append(parsed[2])
+        fromhere.append(parsed[3])
+        has_content.append(len(parsed[2]) > 0)
+
+    df_10k = pd.DataFrame({"date":dates, "content":content, "fromhere":fromhere, "has_content":has_content})
+
+    print(df_10k["fromhere"])
+    '''
+
+    filings_10q = get_filings_by_company(company, "10-Q", 20)
+    # check if enough
+    #if pd.Timestamp(str(filings_10q[1][-1].content["Period of Report"])) > pd.Timestamp(year=2005, month=12, day=31):
+    #    filings_10q = get_filings_by_company(company, "10-Q", 60)
+    #if pd.Timestamp(str(filings_10q[1][-1].content["Period of Report"])) > pd.Timestamp(year=2005, month=12, day=31):
+    #    filings_10q = get_filings_by_company(company, "10-Q", 96)
+
+    dates = []
+    content = []
+    fromhere = []
+    has_content = []
+
+    for i in range(len(filings_10q[0])):
+        period = pd.Timestamp(str(filings_10q[1][i].content["Period of Report"]))
+        if period < start:
+            break
+        parsed = parse_filing(company, filings_10q[0][i], filings_10q[1][i])
+        dates.append(period)
+        content.append(parsed[2])
+        fromhere.append(parsed[3])
+        has_content.append(len(parsed[2]) > 0)
+
+    df_10q = pd.DataFrame({"date": dates, "content": content, "fromhere": fromhere, "has_content": has_content})
+
+    print(df_10q["fromhere"])
+
+
+
 
 #for comapny_id in companies_unique:
 
 df_mo = df_companies[df_companies["company"] == 71]
 
 for i, row in df_mo.iterrows():
-    company = get_company_by_cik(row["cik"])
+    pull_reports(row["cik"], row["ticker"], row["company"], pd.Timestamp(year=2005, month=12, day=31))
+
+
+
+
 
 
 
