066a9ec82a7389d691f3d8b84fc81f7d4f8e925d
LNhart <lukastilmann97@gmail.com>
2020-12-07 19:24:55 +0100

    improved parsing

--- scrape_reports.py
+++ scrape_reports.py
@@ -11,9 +11,9 @@ companies_unique = df_companies["company"].dropna().unique()
 def is_item_1a_header(text):
     if "1A" in text:
         # print(text)
-        text = text.replace("&nbsp", " ").strip().lower()
-        if (len(text) < 30 and "item" in text and "1a" in text) and (
-                len(text) < 15 or ("risk" in text or "factors" in text)):
+        text = text.replace("&nbsp;", " ").strip().lower()
+        if (len(text) < 31 and "tem" in text and "1a" in text) and (
+                len(text) < 13 or ("risk" in text or "factors" in text)):
             # print(text)
             return True
 
@@ -22,28 +22,37 @@ def is_item_1a_header(text):
 
 def is_item_1b_header(text):
     if "1B" in text:
-        text = text.replace("&nbsp", " ").strip().lower()
-        if (len(text) < 50 and "item" in text and "1b" in text) and (
-                len(text) < 15 or ("unresolved" in text or "staff" in text or "comments" in text)):
+        text = text.replace("&nbsp;", " ").strip().lower()
+        if (len(text) < 48 and "tem" in text and "1b" in text) and (
+                len(text) < 13 or ("unresolved" in text or "staff" in text or "comments" in text)):
             return True
 
     return False
 
 
-def is_item_2_header(text):
+def is_10q_item_2_header(text):
     if len(text) < 150 and "2" in text:
-        text = text.replace("&nbsp", " ").strip().lower()
-        if (len(text) < 85 and "item" in text and "2" in text) and (
-                len(text) < 15 or (any(word in text for word in ["unregistered", "equity", "securities", "proceeds"]))):
+        text = text.replace("&nbsp;", " ").strip().lower()
+        if (len(text) < 85 and "tem" in text and "2" in text) and (
+                len(text) < 12 or (any(word in text for word in ["unregistered", "equity", "securities", "proceeds"]))):
+            return True
+
+    return False
+
+def is_10k_item_2_header(text):
+    if len(text) < 150 and "2" in text:
+        text = text.replace("&nbsp;", " ").strip().lower()
+        if (len(text) < 30 and "tem" in text and "2" in text) and (
+                len(text) < 12 or ("properties" in text)):
             return True
 
     return False
 
 def is_item_6_header(text):
     if len(text) < 150 and "6" in text:
-        text = text.replace("&nbsp", " ").strip().lower()
-        if (len(text) < 50 and "item" in text and "6" in text) and (
-                len(text) < 15 or ("exhibits" in text)):
+        text = text.replace("&nbsp;", " ").strip().lower()
+        if (len(text) < 27 and "tem" in text and "6" in text) and (
+                len(text) < 12 or ("exhibits" in text)):
             return True
 
     return False
@@ -52,7 +61,7 @@ def is_item_6_header(text):
 def print_filing(tuple):
   print(str(not tuple[3]) + " " + tuple[0] + " - " + tuple[1] + " -" + tuple[2][:250])
 
-def parse_filing(company, doc_lxml, doc):
+def parse_10q_filing(company, doc_lxml, doc):
     text_elem = doc_lxml.xpath("//text()")
 
     fromhere = False
@@ -63,14 +72,14 @@ def parse_filing(company, doc_lxml, doc):
 
     for text in text_elem:
         if fromhere:
-            if is_item_1b_header(text) or is_item_2_header(text) or is_item_6_header(text):
+            if is_item_1b_header(text) or is_10q_item_2_header(text) or is_item_6_header(text):
                 fromhere = False
                 if len(risk_factors_text) > 10:
                     #print("got item 2 or 1b and has text!")
                     break
                 #else:
                 #    print("got next item but no text!")
-            text = text.strip()
+            text = text.replace("\n", " ").strip()
             if len(text) > 4 and not (
                     any(word in text.lower() for word in ["index", "contents", "item", "factor", "summary"]) and len(
                     text) < 40):
@@ -87,9 +96,44 @@ def parse_filing(company, doc_lxml, doc):
 
     text_content = "\n".join(risk_factors_text)
 
-    #print(fromhere)
-    #print(len(text_content))
-    #print("--------------------------")
+
+    return (company.name, doc.content["Period of Report"], text_content, fromhere)
+
+
+def parse_10k_filing(company, doc_lxml, doc):
+    text_elem = doc_lxml.xpath("//text()")
+
+    fromhere = False
+    current_text = ""
+    risk_factors_text = []
+
+    # print(text_elem)
+
+    for text in text_elem:
+        if fromhere:
+            if is_item_1b_header(text) or is_10k_item_2_header(text):
+                fromhere = False
+                if len(risk_factors_text) > 5:
+                    #print("got item 2 or 1b and has text!")
+                    break
+                #else:
+                #    print("got next item but no text!")
+            text = text.replace("\n", " ").strip()
+            if len(text) > 4 and not (
+                    any(word in text.lower() for word in ["index", "contents", "item", "factor", "summary"]) and len(
+                    text) < 40):
+                if text[0] in ",;:":
+                    current_text = current_text + text
+                else:
+                    current_text = current_text + " " + text
+                if text[-1] in ".!?":
+                    risk_factors_text.append(current_text + " ")
+                    current_text = ""
+        if is_item_1a_header(text):
+            fromhere = True
+            #print("item 1a start")
+
+    text_content = "\n".join(risk_factors_text)
 
     return (company.name, doc.content["Period of Report"], text_content, fromhere)
 
@@ -113,7 +157,6 @@ def pull_reports(cik, ticker, c_id, start):
 
     company = get_company_by_cik(cik)
     #yearly reports first
-    '''
     filings_10k = get_filings_by_company(company, "10-K", 13)
     #check if enough
     if pd.Timestamp(str(filings_10k[1][-1].content["Period of Report"])) > pd.Timestamp(year=2005, month=12, day=31):
@@ -128,7 +171,7 @@ def pull_reports(cik, ticker, c_id, start):
         period = pd.Timestamp(str(filings_10k[1][i].content["Period of Report"]))
         if period < start:
             break
-        parsed = parse_filing(company, filings_10k[0][i], filings_10k[1][i])
+        parsed = parse_10k_filing(company, filings_10k[0][i], filings_10k[1][i])
         dates.append(period)
         content.append(parsed[2])
         fromhere.append(parsed[3])
@@ -136,10 +179,9 @@ def pull_reports(cik, ticker, c_id, start):
 
     df_10k = pd.DataFrame({"date":dates, "content":content, "fromhere":fromhere, "has_content":has_content})
 
-    print(df_10k["fromhere"])
-    '''
+    print(df_10k)
 
-    filings_10q = get_filings_by_company(company, "10-Q", 20)
+    filings_10q = get_filings_by_company(company, "10-K", 20)
     # check if enough
     #if pd.Timestamp(str(filings_10q[1][-1].content["Period of Report"])) > pd.Timestamp(year=2005, month=12, day=31):
     #    filings_10q = get_filings_by_company(company, "10-Q", 60)
@@ -155,7 +197,7 @@ def pull_reports(cik, ticker, c_id, start):
         period = pd.Timestamp(str(filings_10q[1][i].content["Period of Report"]))
         if period < start:
             break
-        parsed = parse_filing(company, filings_10q[0][i], filings_10q[1][i])
+        parsed = parse_10q_filing(company, filings_10q[0][i], filings_10q[1][i])
         dates.append(period)
         content.append(parsed[2])
         fromhere.append(parsed[3])
@@ -166,11 +208,9 @@ def pull_reports(cik, ticker, c_id, start):
     print(df_10q["content"][:250] + " ---- " + df_10q["content"][-250:])
 
 
-
-
 #for comapny_id in companies_unique:
 
-df_mo = df_companies[df_companies["company"] == 71]
+df_mo = df_companies[df_companies["company"] == 42]
 
 for i, row in df_mo.iterrows():
     pull_reports(row["cik"], row["ticker"], row["company"], pd.Timestamp(year=2005, month=12, day=31))
