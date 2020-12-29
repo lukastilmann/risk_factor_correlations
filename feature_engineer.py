58da4d08cb26f322ed4856f7f7bc6fd6a3b65c95
LNhart <lukastilmann97@gmail.com>
2020-12-29 18:40:43 +0100

    solved issues. Now creates risk reports as strings for all companies and quarter ends

--- feature_engineer.py
+++ feature_engineer.py
@@ -4,6 +4,15 @@ file = "data/companies.csv"
 
 df_companies = pd.read_csv(file, sep=";")
 
+def consolidate_reports(df):
+    if len(df[df["has_content"]].index) == 1:
+        return df[df["has_content"]]
+    else:
+        new_content = ""
+        for i, row in df[df["has_content"]].iterrows():
+            new_content += row["content"]
+        new_df = pd.DataFrame({"content": [new_content]}, index=df.iloc[0].index)
+        return new_df
 
 def create_columns():
     companies = df_companies.loc[:,["company", "ticker"]].dropna().drop_duplicates()
@@ -19,45 +28,70 @@ def create_columns():
         location_10k = file_format.format(ticker=row["ticker"], id=row["company"], type="10-K")
         location_10q = file_format.format(ticker=row["ticker"], id=row["company"], type="10-Q")
 
-        df_10k = pd.read_csv(location_10k, index_col="Unnamed: 0").sort_index()
-        df_10k.index = pd.to_datetime(df_10k.index)
-        df_10q = pd.read_csv(location_10q, index_col="Unnamed: 0").sort_index()
-        df_10q.index = pd.to_datetime(df_10q.index)
+        df_10k = pd.read_csv(location_10k).drop_duplicates()
+        df_10k.index = df_10k.index = pd.to_datetime(df_10k["Unnamed: 0"])
+        df_10k = df_10k.sort_index()
+        df_10q = pd.read_csv(location_10q).drop_duplicates()
+        df_10q.index = df_10q.index = pd.to_datetime(df_10q["Unnamed: 0"])
+        df_10q = df_10q.sort_index()
 
         reports = []
         dates = []
+        last_date = df_10q.index[-1]
 
-        for date in df_10k.index:
+        for date in df_10k.index.drop_duplicates():
             quarter_end_date = date - pd.tseries.offsets.DateOffset(days=1) + pd.tseries.offsets.QuarterEnd()
+            quarter_start_date = date - pd.offsets.QuarterBegin() + pd.Timedelta(days=1)
 
-            report_10k = df_10k.loc[:quarter_end_date].iloc[-1]["content"]
-            # print(report_10k)
-            reports.append(report_10k)
-            dates.append(quarter_end_date)
+            df_date = df_10k.loc[quarter_start_date:quarter_end_date]
 
-            last_date = df_10q.index[-1]
+            length = len(df_date)
 
-            for i in range(3):
-                quarter_end_date = quarter_end_date + pd.Timedelta(days=1) + pd.tseries.offsets.QuarterEnd()
-                if last_date < quarter_end_date:
-                    break
-                report = df_10q.loc[:quarter_end_date].iloc[-1]["content"]
-                if not pd.isna(report):
-                    content = report_10k + report
-                    reports.append(content)
-                    dates.append(quarter_end_date)
+            if length == 1:
+                report_10k = df_date.iloc[0]["content"]
 
-        df = pd.Series(reports, index=dates)
+            elif length > 1:
+                df_date = consolidate_reports(df_date)
+                report_10k = df_date.iloc[0]["content"]
+            else:
+                report_10k = pd.NA
 
-        print(df[df.index.duplicated()])
+            if not pd.isna(report_10k):
+                reports.append(report_10k)
+                dates.append(quarter_end_date)
 
-            #df_reports[row["ticker"] + "_" + str(row["company"])] = df
+                for i in range(3):
 
-        ind += 1
-        if ind >4:
-            break
+                    quarter_start_date = quarter_end_date + pd.Timedelta(days=1)
+                    quarter_end_date = quarter_start_date + pd.tseries.offsets.QuarterEnd()
+                    if last_date < quarter_end_date:
+                        break
+                    last_quarter_df = df_10q.loc[quarter_start_date:quarter_end_date]
+                    length = len(last_quarter_df.index)
+                    if length == 1:
+                        report = last_quarter_df.iloc[0]["content"]
 
-    #print(df_reports)
+                    elif length > 1:
+                        last_quarter_df = consolidate_reports(last_quarter_df)
+                        report = last_quarter_df.iloc[0]["content"]
+
+                    else:
+                        report = pd.NA
+                    # report = df_10q.loc[quarter_end_date:new_quarter_end_date].iloc[-1]["content"]
+
+                    if not pd.isna(report):
+                        content = report_10k + report
+                        reports.append(content)
+                        dates.append(quarter_end_date)
+
+
+        if len(reports) > 0:
+            df = pd.Series(reports, index=dates)
+            if len(df[df.index.duplicated()]) == 0:
+                df_reports[row["ticker"] + "_" + str(row["company"])] = df
+
+
+    print(df_reports)
 
 
 
