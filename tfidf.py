531fb17ac7b1fc438c04b95d89e0038fbdff09c9
LNhart <lukastilmann97@gmail.com>
2021-01-05 23:33:09 +0100

    improved function to find intersections of columns, implemented benchmark prediction and prediction based on document similarity

--- tfidf.py
+++ tfidf.py
@@ -41,22 +41,69 @@ def get_returns_for_period(df, start, stop):
 
 def compute_cov_matrix(returns):
 
-    matrix = np.corrcoef(returns.values.transpose())
+    matrix = np.cov(returns.values.transpose())
 
     df = pd.DataFrame(matrix, index=returns.columns, columns=returns.columns)
 
     return df
 
-def returns_reports_in_both(returns, reports):
+def df_find_intersection(list_dfs):
 
-    returns_companies = returns.columns
-    reports_companies = reports.columns
+    list_columns = []
 
-    in_both = returns_companies.intersection(reports_companies)
+    for df in list_dfs:
+        list_columns.append(set(df.columns))
 
-    #print(in_both)
+    in_all = set.intersection(*list_columns)
 
-    return returns.loc[in_both, in_both], reports.loc[in_both, in_both]
+    list_dfs_new = []
+
+    for df in list_dfs:
+        list_dfs_new.append(df.loc[in_all,in_all])
+
+    return list_dfs_new
+
+
+def predict_cov_sample(prev_sample):
+
+    return compute_cov_matrix(prev_sample).values
+
+
+def predict_cov_cos_similarity(prev_sample, similarities):
+
+    prev_sample = prev_sample.values
+
+    similarities = similarities.values
+
+    mean_cov = np.mean(prev_sample)
+
+    sigma_cov = np.std(prev_sample)
+
+    mean_sim = np.mean(similarities)
+
+    sigma_sim = np.std(similarities)
+
+    c = (mean_sim - similarities) / sigma_sim
+
+    #print(c)
+
+    pred = mean_cov - c * sigma_cov
+
+    return pred
+
+def predict_mean(prev_sample):
+
+    cov_mat = prev_sample.values
+    without_diag = cov_mat[~np.eye(cov_mat.shape[0], dtype=bool)].reshape(cov_mat.shape[0], -1)
+    mean_cov = np.mean(without_diag)
+    mean_cov = np.full(cov_mat.shape, mean_cov)
+    mean_variance = np.mean(np.diagonal(cov_mat))
+    np.fill_diagonal(mean_cov, mean_variance)
+
+    return mean_cov
+
+def eval_predictions(true, pred):
+    return (np.square(true - pred)).mean(axis=None)
 
 
 
@@ -71,22 +118,34 @@ else:
     pickle.dump(vectorizer, open("vectorizer.p", "wb"))
 
 reports = get_reports_for_date(df, "31-12-2006")
-
-similarities = compute_cosine_similarity_matrix(reports)
-
 returns = pd.read_csv("data/stock_returns.csv", index_col="Date")
 returns.index = pd.to_datetime(returns.index)
 
-returns_period = get_returns_for_period(returns, "01-01-2007", "31-03-2007")
+similarities = compute_cosine_similarity_matrix(reports)
 
+returns_period = get_returns_for_period(returns, "01-01-2007", "31-03-2007")
 cov = compute_cov_matrix(returns_period)
 
-cov, similarities = returns_reports_in_both(cov, similarities)
+returns_prev_quarter = get_returns_for_period(returns, "01-10-2006", "31-12-2006")
+cov_prev_sample = compute_cov_matrix(returns_prev_quarter)
 
-cov = cov.values.flatten()
-similarities = similarities.values.flatten()
+cov, similarities, cov_prev_sample = df_find_intersection([cov, similarities, cov_prev_sample])
 
-print(np.corrcoef(cov, similarities))
+
+#returns_prev_quarter, similarities = returns_reports_in_both(returns_prev_quarter, similarities)
+#pred_sample = predict_cov_sample(returns_prev_quarter)
+
+pred_sim = predict_cov_cos_similarity(cov_prev_sample, similarities)
+pred_mean = predict_mean(cov_prev_sample)
+
+print(eval_predictions(cov.values, cov_prev_sample.values))
+print(eval_predictions(cov.values, pred_sim))
+print(eval_predictions(cov.values, pred_mean))
+
+#cov = cov.values.flatten()
+#similarities = similarities.values.flatten()
+
+#print(np.corrcoef(cov, similarities))
 #print(returns_reports_in_both(cov, similarities)[0].shape)
 #print(returns_reports_in_both(cov, similarities)[1].shape)
 
